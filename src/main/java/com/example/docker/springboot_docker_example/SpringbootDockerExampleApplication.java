14ab6c09e7345e8fa09525382c9b9d6c0e543886
shakeelkhangit <mailtoshakeel786@gmail.com>
2025-07-07 16:18:00 +0530

    My First Development on Docker

---
+++ src/main/java/com/example/docker/springboot_docker_example/SpringbootDockerExampleApplication.java
@@ -0,0 +1,21 @@
+package com.example.docker.springboot_docker_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootDockerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerExampleApplication.class, args);
+	}
+
+	@GetMapping("/message")
+	public String showMessage(){
+		return "Congratulation you have deployed first application on Docker!!!";
+	}
+
+}
---
+++ src/test/java/com/example/docker/springboot_docker_example/SpringbootDockerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docker.springboot_docker_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
