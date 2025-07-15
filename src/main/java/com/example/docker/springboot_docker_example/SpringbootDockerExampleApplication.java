9db2058ba954a8b5f21484decea8df0b88e9e1b4
shakeelkhangit <mailtoshakeel786@gmail.com>
2025-07-15 23:35:46 +0530

    Modify the pom,xml file1111333334444

--- src/main/java/com/example/docker/springboot_docker_example/SpringbootDockerExampleApplication.java
+++ src/main/java/com/example/docker/springboot_docker_example/SpringbootDockerExampleApplication.java
@@ -15,7 +15,7 @@ public class SpringbootDockerExampleApplication {
 
 	@GetMapping("/message")
 	public String showMessage(){
-		return "Congratulation you have deployed first application on Docker by Jenkins issues Resolved luckey !!!!!";
+		return "Congratulation you have deployed first application on Docker by Jenkins issues hhh !!!!!";
 	}
 
 }
